d34c84125cac90d1003576340f6ba18d7269b73e
rahul9561 <109431579+rahul9561@users.noreply.github.com>
2024-10-01 13:05:31 +0530

    vercel setup

--- my_project/settings.py
+++ my_project/settings.py
@@ -120,12 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-    # Add more directories if needed
-]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
