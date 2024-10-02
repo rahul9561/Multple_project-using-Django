4e1bf07fb9775b0462689bfb84192f00a77273da
rahul9561 <109431579+rahul9561@users.noreply.github.com>
2024-10-02 10:53:05 +0530

    router

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&q4ryxj-vxjvm*syuhm_c4hh0x2bsjp=8ebvkop(y389e*)h-a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
